e417946f30d27fbb7dd8827ff171bbb5c923ee4b
dyushkin93 <d.yushkin93@gmail.com>
2020-03-07 23:38:10 +0500

    feat: complete task, test passed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array==undefined || array.length==0) {
+    return 0;
+  } else {
+    array=array.reduce((min, elem) => {
+      if (elem < min) {
+        return min=elem;
+      } else {
+        return min;
+      }
+    }, array[0])
+    return array;
+  } 
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array==undefined || array.length==0) {
+    return 0;
+  } else {
+    array=array.reduce((max, elem) => {
+      if (elem > max) {
+        return max=elem;
+      } else {
+        return max;
+      }
+    }, array[0])
+    return array;
+  } 
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array==undefined || array.length==0) {
+    return 0;
+  } else {
+    let arrLength=array.length;
+    array=array.reduce((avg, elem, i, arr) => {
+      return avg+elem;
+    })
+    return array/arrLength;
+  }
 }
